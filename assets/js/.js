ecf347a9b6978650b76c36bb2ced58f73c49e9aa
pjshvision <peter.og.shearman@gmail.com>
2020-05-16 21:20:43 +0000

    Attempting to fix responsive logo with demo code

--- assets/js/.js
+++ assets/js/.js
@@ -1,16 +1,8 @@
-let button = document.querySelector('.button');
-let buttonText = document.querySelector('.tick');
+var navList = document.getElementById("nav-lists");
+function Show() {
+navList.classList.add("_Menus-show");
+}
 
-const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
-
-buttonText.innerHTML = "Submit";
-
-button.addEventListener('click', function() {
-
-  if (buttonText.innerHTML !== "Submit") {
-    buttonText.innerHTML = "Submit";
-  } else if (buttonText.innerHTML === "Submit") {
-    buttonText.innerHTML = tickMark;
-  }
-  this.classList.toggle('button__circle');
-});
+function Hide(){
+navList.classList.remove("_Menus-show");
+}
